4e3ff8f86bcdd0434c349ed1b2d967dc02d15843
chen6665758 <332965198@qq.com>
2020-01-10 16:24:48 +0800

    为按钮添加权限

--- app/src/main/java/com/cg/baseandroidx/MainActivity.java
+++ app/src/main/java/com/cg/baseandroidx/MainActivity.java
@@ -1,13 +1,17 @@
 package com.cg.baseandroidx;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cg.baseandroidx.Adapters.tempBannerViewHolder;
+import com.cg.baseandroidx.infos.Constants;
 import com.cg.baseandroidx.models.tempBannerModel;
 import com.zhpan.bannerview.BannerViewPager;
 import com.zhpan.bannerview.constants.IndicatorGravity;
@@ -17,14 +21,21 @@ import com.zhpan.bannerview.holder.HolderCreator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseActivity {
+import androidx.annotation.NonNull;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends BaseActivity implements EasyPermissions.PermissionCallbacks {
 
     /**
      * banner
      */
     private BannerViewPager<tempBannerModel, tempBannerViewHolder> mBannerViewPager;
 
-    private Button btn_tempFragment;
+    //权限参数
+    private int PermissionsCode = 1000;
+    private List<String> perms;
+
+    private TextView btn_tempFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +56,25 @@ public class MainActivity extends BaseActivity {
      */
     private void initControls()
     {
+        //权限认证
+        perms = new ArrayList<>();
+        perms.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        perms.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        hasPermissions();
+
         //banner的初始化
         mBannerViewPager = (BannerViewPager)findViewById(R.id.banner_view);
         initBanner();
 
-        btn_tempFragment = (Button)findViewById(R.id.btn_tempFragment);
+        btn_tempFragment = (TextView)findViewById(R.id.btn_tempFragment);
         btn_tempFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Jump_intent(tempFragmentActivity.class,null);
+                if(EasyPermissions.hasPermissions(mContext, Constants.PERMISSION_WRITEREAD)) {
+                    Jump_intent(tempFragmentActivity.class, null);
+                }else{
+                    hasPermissions();
+                }
             }
         });
     }
@@ -119,6 +140,60 @@ public class MainActivity extends BaseActivity {
         super.onResume();
         if (mBannerViewPager != null)
             mBannerViewPager.startLoop();
+    }
+
+
+
+    //检查自己是否申请过权限
+    private void hasPermissions() {
+        if (!EasyPermissions.hasPermissions(this, Constants.PERMISSION_WRITEREAD)) {
+
+            Log.e("tempFragmentActivity", "行数: 79  这里没有走吗！");
+            //EasyPermissions.requestPermissions(new PermissionRequest.Builder(this, PermissionsCode, Constants.PERMISSION_WRITEREAD).build());
+            //如果用户没有点 不再提示，则此处会给出提示，否则不会走
+            //此方法是控件自带，可以在这里写自己的方法
+            EasyPermissions.requestPermissions(this, "您拒绝了读取文件的功能，会造成软件无法更新下载,并且会影响到维修与巡更功能！",
+                    PermissionsCode,  Constants.PERMISSION_WRITEREAD);
+        }
+    }
+
+    //权限受给了
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+    //权限没有被受给
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull final List<String> perms) {
+
+        Log.e("MainActivity.java(onPermissionsDenied)", "行数: 169  没给权限");
+        //否认，弹出对话框，直到用户允许此权限为止，才不会走此方法。
+//        new AlertDialog.Builder(this)
+//                .setMessage("打开此")
+//                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        /**
+//                         * 若是在权限弹窗中，用户勾选了'不在提示'，且拒绝权限。
+//                         * 这时候，需要跳转到设置界面去，让用户手动开启。
+//                         */
+//
+//
+//                    }
+//                }).setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialogInterface, int i) {
+//                    hasPermissions();
+//                }
+//        }).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // 将结果转发到easypermissions
+        Log.e("MainActivity", "行数::" + requestCode + "--" + grantResults.toString());
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
 
